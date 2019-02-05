2edfc25bb1379054a6626cd7abaedb08e53e928b
yaozeyuan <yaozeyuan001@lianjia.com>
2019-02-05 20:13:38 +0800

    feat: 添加获取文章详情列表接口

--- src/api/column.ts
+++ src/api/column.ts
@@ -1,6 +1,7 @@
 import _ from "lodash";
 import Base from "~/src/api/base";
 import ColumnRecord from '~/src/type/model/column'
+import ArticleExcerptRecord from '~/src/type/model/article_excerpt'
 
 class Column extends Base {
     /**
@@ -16,6 +17,23 @@ class Column extends Base {
         });
         const columnInfo = _.get(record, ["data"], {});
         return columnInfo;
+    }
+
+    /**
+     * 获取专栏信息
+     * @param columnId
+     */
+    static async asyncGetAtricleExcerptList(columnId: string, offset = 0, limit = 10): Promise<Array<ArticleExcerptRecord>> {
+        const baseUrl = `https://zhuanlan.zhihu.com/api2/columns/${columnId}/articles`;
+        const config = {
+            offset,
+            limit
+        };
+        const record = await Base.http.get(baseUrl, {
+            params: config
+        });
+        const atricleExcerptList = _.get(record, ["data"], {});
+        return atricleExcerptList;
     }
 }
 export default Column;
