e92c9f1d4ae5964ace8c21ba512313194be6bf73
yaozeyuan <yaozeyuan001@lianjia.com>
2019-02-08 23:45:46 +0800

    feat: 使用命名空间声明类型

--- src/command/generate/activity.tsx
+++ src/command/generate/activity.tsx
@@ -49,52 +49,50 @@ class GenerateActivity extends Base {
                 this.log(`${displayKey} => ${count}次`)
             }
         }
-        return
 
 
-        // this.log(`用户${name}(${urlToken})共有${answerCount}个回答`)
-        // const bookname = StringUtil.encodeFilename(`用户${name}(${urlToken})的知乎回答集锦`)
-        // // 初始化文件夹
-        // this.log(`创建电子书:${bookname}对应文件夹`)
-        // let htmlCachePath = path.resolve(PathConfig.htmlCachePath, bookname)
-        // let htmlCacheHtmlPath = path.resolve(htmlCachePath, '') // 单文件就没必要放在html文件夹内了, 直接放最外层即可
-        // let htmlCacheCssPath = path.resolve(htmlCachePath, 'css')
-        // let htmlCacheImgPath = path.resolve(htmlCachePath, 'image')
-        // shelljs.mkdir('-p', htmlCachePath)
-        // shelljs.mkdir('-p', htmlCacheHtmlPath)
-        // shelljs.mkdir('-p', htmlCacheCssPath)
-        // shelljs.mkdir('-p', htmlCacheImgPath)
-        // this.log(`电子书:${bookname}对应文件夹创建完毕`)
+        const bookname = StringUtil.encodeFilename(`用户${name}(${urlToken})的知乎故事`)
+        // 初始化文件夹
+        this.log(`创建电子书:${bookname}对应文件夹`)
+        let htmlCachePath = path.resolve(PathConfig.htmlCachePath, bookname)
+        let htmlCacheHtmlPath = path.resolve(htmlCachePath, '') // 单文件就没必要放在html文件夹内了, 直接放最外层即可
+        let htmlCacheCssPath = path.resolve(htmlCachePath, 'css')
+        let htmlCacheImgPath = path.resolve(htmlCachePath, 'image')
+        shelljs.mkdir('-p', htmlCachePath)
+        shelljs.mkdir('-p', htmlCacheHtmlPath)
+        shelljs.mkdir('-p', htmlCacheCssPath)
+        shelljs.mkdir('-p', htmlCacheImgPath)
+        this.log(`电子书:${bookname}对应文件夹创建完毕`)
 
-        // this.log(`获取回答列表`)
-        // let answerRecordList = await MAnswer.asyncGetAnswerList(urlToken)
-        // this.log(`回答列表获取完毕, 共${answerRecordList.length}条答案`)
-        // // 直接渲染为单个文件
-        // let content = renderAnswer(bookname, authorInfo, answerRecordList)
-        // this.log(`回答渲染完毕, 开始对内容进行输出前预处理`)
-        // content = this.processContent(content)
-        // fs.writeFileSync(path.resolve(htmlCacheHtmlPath, `${bookname}.html`), content)
-        // this.log(`回答列表预处理完毕, 准备下载图片`)
-        // // 下载图片
-        // await this.downloadImg()
-        // this.log(`图片下载完毕`)
-        // this.copyImgToCache(htmlCacheImgPath)
+        this.log(`获取赞同记录列表`)
+        let activityRecordList = await MActivity.asyncGetActivityList(urlToken, actionStartAt, actionEndAt)
+        this.log(`回答列表获取完毕, 共${activityRecordList.length}条答案`)
+        // 直接渲染为单个文件
+        let content = renderAnswer(bookname, authorInfo, activityRecordList)
+        this.log(`回答渲染完毕, 开始对内容进行输出前预处理`)
+        content = this.processContent(content)
+        fs.writeFileSync(path.resolve(htmlCacheHtmlPath, `${bookname}.html`), content)
+        this.log(`回答列表预处理完毕, 准备下载图片`)
+        // 下载图片
+        await this.downloadImg()
+        this.log(`图片下载完毕`)
+        this.copyImgToCache(htmlCacheImgPath)
 
-        // this.log(`复制静态资源`)
-        // // css
-        // for (let filename of ['bootstrap.css', 'customer.css', 'markdown.css', 'normalize.css',]) {
-        //     let copyFromUri = path.resolve(PathConfig.resourcePath, 'css', filename)
-        //     let copyToUri = path.resolve(htmlCacheCssPath, filename)
-        //     fs.copyFileSync(copyFromUri, copyToUri)
-        // }
-        // // 图片资源
-        // for (let filename of ['cover.jpg', 'kanshan.png']) {
-        //     let copyFromUri = path.resolve(PathConfig.resourcePath, 'image', filename)
-        //     let copyToUri = path.resolve(htmlCacheImgPath, filename)
-        //     fs.copyFileSync(copyFromUri, copyToUri)
-        // }
+        this.log(`复制静态资源`)
+        // css
+        for (let filename of ['bootstrap.css', 'customer.css', 'markdown.css', 'normalize.css',]) {
+            let copyFromUri = path.resolve(PathConfig.resourcePath, 'css', filename)
+            let copyToUri = path.resolve(htmlCacheCssPath, filename)
+            fs.copyFileSync(copyFromUri, copyToUri)
+        }
+        // 图片资源
+        for (let filename of ['cover.jpg', 'kanshan.png']) {
+            let copyFromUri = path.resolve(PathConfig.resourcePath, 'image', filename)
+            let copyToUri = path.resolve(htmlCacheImgPath, filename)
+            fs.copyFileSync(copyFromUri, copyToUri)
+        }
 
-        // this.log(`作者${name}(${urlToken})的知乎回答集锦制作完毕`)
+        this.log(`作者${name}(${urlToken})的知乎回答集锦制作完毕`)
     }
 
 }
