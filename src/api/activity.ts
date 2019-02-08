691301ec66ae95fdd6155a62a88c0421dc9e4b10
yaozeyuan <yaozeyuan001@lianjia.com>
2019-02-08 22:32:52 +0800

    feat: 行为抓取命令调试完毕

--- src/api/activity.ts
+++ src/api/activity.ts
@@ -1,6 +1,7 @@
 import _ from 'lodash'
 import Base from '~/src/api/base'
 import ActivityRecord from '~/src/type/model/activity'
+import moment from 'moment'
 
 class Activity extends Base {
   /**
@@ -23,6 +24,44 @@ class Activity extends Base {
     })
     const activityList = _.get(record, ['data'], [])
     return activityList
+  }
+
+  /**
+  * 检查指定时间后是否还有用户活跃记录 
+  * @param url_token
+  * @param afterTimeAt 从X时间后
+  */
+  static async asyncCheckHasAutherActivityAfterAt(url_token: string, afterTimeAt: number = 0): Promise<boolean> {
+    const baseUrl = `https://www.zhihu.com/api/v4/members/${url_token}/activities`
+    const config = {
+      after_id: afterTimeAt,
+      limit: 10,
+      desktop: 'True',
+    }
+    const record = await Base.http.get(baseUrl, {
+      params: config
+    })
+    const hasActivity = _.get(record, ['paging', 'is_end'], false)
+    return hasActivity === false
+  }
+
+  /**
+  * 获取用户最近一次活跃时间 
+  * @param url_token
+  */
+  static async asyncGetAutherLastActivityAt(url_token: string): Promise<number> {
+    const baseUrl = `https://www.zhihu.com/api/v4/members/${url_token}/activities`
+    let now = moment().unix()
+    const config = {
+      after_id: now,
+      limit: 10,
+      desktop: 'True',
+    }
+    const record = await Base.http.get(baseUrl, {
+      params: config
+    })
+    const lastActivityAt = _.get(record, ['data', 0, 'id'], now)
+    return lastActivityAt
   }
 }
 export default Activity
--- src/command/fetch/activity.ts
+++ src/command/fetch/activity.ts
@@ -8,7 +8,7 @@ import DATE_FORMAT from "~/src/constant/date_format";
 import CommonUtil from "~/src/library/util/common";
 
 
-class FetchAuthor extends Base {
+class FetchActivity extends Base {
     max = 20
 
     static get signature() {
@@ -33,15 +33,36 @@ class FetchAuthor extends Base {
         const name = authorInfo.name
         this.log(`开始抓取用户行为列表`)
         let startAt = MActivity.ZHIHU_ACTIVITY_START_MONTH_AT
-        let endAt = MActivity.ZHIHU_ACTIVITY_END_MONTH_AT
-        this.log(`抓取时间范围为:${moment.unix(startAt).format(DATE_FORMAT.DISPLAY_BY_SECOND)} ~ ${moment.unix(endAt).format(DATE_FORMAT.DISPLAY_BY_SECOND)}, 按月抓取`)
+        this.log(`检查用户${name}(${urlToken})最近一次活跃时间`)
+        let endAt = await ActivityApi.asyncGetAutherLastActivityAt(urlToken)
+        this.log(`用户${name}(${urlToken})最后一次活跃于${moment.unix(endAt).format(DATE_FORMAT.DISPLAY_BY_SECOND)}`)
+
+        this.log(`检查用户${name}(${urlToken})首次活跃时间`)
+        for (
+            let checkAt = startAt;
+            checkAt <= endAt;
+        ) {
+            let hasActivityAfterAt = await ActivityApi.asyncCheckHasAutherActivityAfterAt(urlToken, checkAt)
+            if (hasActivityAfterAt) {
+                this.log(`经检查, 用户${name}(${urlToken})在${moment.unix(checkAt).format(DATE_FORMAT.DISPLAY_BY_SECOND)}前有活动记录`)
+                this.log(`检查完毕`)
+                startAt = moment.unix(checkAt).startOf(DATE_FORMAT.UNIT.MONTH).unix()
+                break
+            } else {
+                this.log(`经检查, 用户${name}(${urlToken})在${moment.unix(checkAt).format(DATE_FORMAT.DISPLAY_BY_SECOND)}前没有活动记录`)
+                this.log(`向后推一个月, 继续检查`)
+                let newCheckAt = moment.unix(checkAt).add(1, DATE_FORMAT.UNIT.MONTH).unix()
+                checkAt = newCheckAt
+            }
+        }
+        this.log(`用户活动时间范围为${moment.unix(startAt).format(DATE_FORMAT.DISPLAY_BY_SECOND)} ~ ${moment.unix(endAt).format(DATE_FORMAT.DISPLAY_BY_SECOND)}, 按照该范围按月抓取`)
         for (let fetchAt = startAt; fetchAt <= endAt;) {
             let fetchStartAt = fetchAt
             let fetchEndAt = moment.unix(fetchAt).endOf(DATE_FORMAT.UNIT.MONTH).unix()
             fetchAt = fetchEndAt + 1
-            await this.fetchActivityInRange(urlToken, startAt, fetchEndAt)
-            // await CommonUtil.appendPromiseWithDebounce(
-            // )
+            await CommonUtil.appendPromiseWithDebounce(
+                this.fetchActivityInRange(urlToken, fetchStartAt, fetchEndAt)
+            )
         }
     }
 
@@ -58,6 +79,11 @@ class FetchAuthor extends Base {
         for (let fetchAt = endAt; fetchAt >= startAt;) {
             this.log(`[${rangeString}]抓取${moment.unix(fetchAt).format(DATE_FORMAT.DISPLAY_BY_SECOND)}的记录`)
             let activityList = await ActivityApi.asyncGetAutherActivityList(urlToken, fetchAt)
+            if (activityList.length === 0) {
+                // 没有这段时间的记录或者接口调用失败, 自动往前挪一天
+                fetchAt = fetchAt - 86400
+                continue
+            }
             for (let activityRecord of activityList) {
                 activityCounter = activityCounter + 1
                 // 更新时间
@@ -70,4 +96,4 @@ class FetchAuthor extends Base {
 
 }
 
-export default FetchAuthor;
+export default FetchActivity;
