8f4074b0bf4d9d01918ba345668fd69eb26d1220
yaozeyuan <yaozeyuan001@lianjia.com>
2019-02-10 22:07:37 +0800

    fix: xml类型声明必须放在首行, 之前不能有空格

--- src/library/epub/toc.ts
+++ src/library/epub/toc.ts
@@ -12,19 +12,19 @@ class TOC {
     navMapList: Array<string> = []
 
     get content() {
-        return `
-        <?xml version="1.0"?>
-        <html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops">
-        <head>
-            <title>${this.title}</title>
-        </head>
-        <body>
-          <nav id="toc" epub:type="toc">
-            <ol>
-                ${this.navOlString}
-            </ol>
-          </nav>
-        </body></html>
+        return `<?xml version="1.0"?>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops">
+<head>
+    <title>${this.title}</title>
+</head>
+<body>
+  <nav id="toc" epub:type="toc">
+    <ol>
+        ${this.navOlString}
+    </ol>
+  </nav>
+</body>
+</html>
 `
 
     }
