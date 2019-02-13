46b519b0ff873e9d99354650ff05e7700311c85c
yaozeyuan <yaozeyuan001@lianjia.com>
2019-02-13 22:09:56 +0800

    feat: 准备支持在图形界面中打印log

--- src/library/logger.ts
+++ src/library/logger.ts
@@ -1,14 +1,49 @@
 import moment from 'moment'
+import _ from 'lodash'
 import DATE_FORMAT from '~/src/constant/date_format'
-class Logger {
-  static log(...arg: Array<any>) {
-    const triggerAt = moment().format(DATE_FORMAT.DISPLAY_BY_MILLSECOND)
 
-    console.log(`${triggerAt}:`, ...arg)
-  }
-  static warn(...arg: Array<any>) {
+class Logger {
+  private static formatArgument (...arg: Array<any>) {
     const triggerAt = moment().format(DATE_FORMAT.DISPLAY_BY_MILLSECOND)
-    console.warn(`${triggerAt}:`, ...arg)
+    let stringLogItemList = []
+    for (let logItem of [...arg]) {
+      if (_.isString(logItem)) {
+        stringLogItemList.push(` ${logItem} `)
+      } else {
+        stringLogItemList.push(JSON.stringify(logItem, null, 4))
+      }
+    }
+    let logContent = `${triggerAt}:` + stringLogItemList.join('')
+    return logContent
+  }
+
+  private static pushLogContentToGlobal (logContent: string) {
+    if (global === undefined) {
+      return
+    }
+
+    let logList: Array<string> = global.logList
+    if (logList === undefined) {
+      logList = []
+    }
+    logList.push(logContent)
+    if (logList.length >= 1000) {
+      logList = logList.slice(logList.length - 1000, logList.length)
+    }
+    global.logList = logList
+    return
+  }
+
+  static log (...arg: Array<any>) {
+    let logContent = Logger.formatArgument(...arg)
+    // 将日志存入Electron全局变量中
+    Logger.pushLogContentToGlobal(logContent)
+    console.log(logContent)
+  }
+  static warn (...arg: Array<any>) {
+    let logContent = Logger.formatArgument(...arg)
+    Logger.pushLogContentToGlobal(logContent)
+    console.warn(logContent)
   }
 }
 
