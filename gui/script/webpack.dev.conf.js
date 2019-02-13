6a8e3e6653d7a3c65c1498e26365297581371f05
yaozeyuan <yaozeyuan001@lianjia.com>
2019-02-13 22:43:49 +0800

    fix: 逗号漏掉了

--- gui/script/webpack.dev.conf.js
+++ gui/script/webpack.dev.conf.js
@@ -13,8 +13,8 @@ module.exports = merge(baseWebpackConfig, {
   mode: 'development',
   output:{
     // 本地调试时应该是绝对路径
-    publicPath: '/'
-  }
+    publicPath: '/',
+  },
   // cheap-module-eval-source-map is faster for development
   devtool: '#source-map', // eval-source-map只能看，不能调试，得不偿失
   plugins: [
