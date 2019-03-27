ecff06635be0a41eeb3a0101e59236cf2f692053
yaozeyuan <yaozeyuan001@lianjia.com>
2019-03-28 01:21:43 +0800

    feat: 添加待统一的参数demo

--- src/api/answer.ts
+++ src/api/answer.ts
@@ -13,13 +13,14 @@ class Answer extends Base {
   static async asyncGetAutherAnswerList(url_token: string, offset: number = 0, limit: number = 20, sortBy: string = Base.CONST_SORT_BY_CREATED): Promise<Array<TypeAnswer.Record>> {
     const baseUrl = `https://www.zhihu.com/api/v4/members/${url_token}/answers`
     const config = {
-      include: 'data[*].is_normal,admin_closed_comment,reward_info,is_collapsed,annotation_action,annotation_detail,collapse_reason,collapsed_by,suggest_edit,comment_count,can_comment,content,voteup_count,reshipment_settings,comment_permission,mark_infos,created_time,updated_time,review_info,question,excerpt,is_labeled,label_info,relationship.is_authorized,voting,is_author,is_thanked,is_nothelp;data[*].author.badge[?(type=best_answerer)].topics',
+      include:
+        'data[*].is_normal,admin_closed_comment,reward_info,is_collapsed,annotation_action,annotation_detail,collapse_reason,collapsed_by,suggest_edit,comment_count,can_comment,content,voteup_count,reshipment_settings,comment_permission,mark_infos,created_time,updated_time,review_info,question,excerpt,is_labeled,label_info,relationship.is_authorized,voting,is_author,is_thanked,is_nothelp;data[*].author.badge[?(type=best_answerer)].topics',
       offset: offset,
       limit: limit,
-      sort_by: sortBy
+      sort_by: sortBy,
     }
     const record = await Base.http.get(baseUrl, {
-      params: config
+      params: config,
     })
     const answerList = _.get(record, ['data'], [])
     return answerList
@@ -27,15 +28,17 @@ class Answer extends Base {
 
   /**
    * 获取单个回答
-   * @param id 
+   * @param id
    */
   static async asyncGetAnswer(id: number | string): Promise<TypeAnswer.Record> {
     const baseUrl = `https://api.zhihu.com/answers/${id}`
     const config = {
-      include: `data[*].is_normal,admin_closed_comment,reward_info,is_collapsed,annotation_action,annotation_detail,collapse_reason,collapsed_by,suggest_edit,comment_count,can_comment,content,voteup_count,reshipment_settings,comment_permission,mark_infos,created_time,updated_time,review_info`
+      include: `data[*].is_normal,admin_closed_comment,reward_info,is_collapsed,annotation_action,annotation_detail,collapse_reason,collapsed_by,suggest_edit,comment_count,can_comment,content,voteup_count,reshipment_settings,comment_permission,mark_infos,created_time,updated_time,review_info`,
+      // @todo(yaozeyuan) 周末把API集中替换过来
+      // 更好的参数(可以获得问题详情) =>  'data[*].is_normal,admin_closed_comment,reward_info,is_collapsed,annotation_action,annotation_detail,collapse_reason,is_sticky,collapsed_by,suggest_edit,comment_count,can_comment,content,editable_content,voteup_count,reshipment_settings,comment_permission,mark_infos,created_time,updated_time,review_info,question.detail,answer_count,follower_count,excerpt,detail,question_type,title,id,created,updated_time,relevant_info,excerpt,label_info,relationship.is_authorized,is_author,voting,is_thanked,is_nothelp,is_labeled,is_recognized',
     }
     const answerRecord: TypeAnswer.Record = await Base.http.get(baseUrl, {
-      params: config
+      params: config,
     })
     return answerRecord
   }
