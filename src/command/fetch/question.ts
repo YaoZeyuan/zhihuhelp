c03a9d9fcbe2943ca4f79a5f57e462f75e940615
yaozeyuan <yaozeyuan001@lianjia.com>
2019-04-06 10:14:24 +0800

    feat: 抓取单个问题功能初始化完毕

--- src/command/fetch/question.ts
+++ src/command/fetch/question.ts
@@ -1,46 +1,42 @@
-import Base from "~/src/command/fetch/base";
-import AuthorApi from "~/src/api/author";
-import AnswerApi from "~/src/api/answer";
-import MAnswer from "~/src/model/answer";
-import MAuthor from "~/src/model/author";
+import Base from '~/src/command/fetch/base'
+import QuestionApi from '~/src/api/question'
+import AnswerApi from '~/src/api/answer'
+import MAnswer from '~/src/model/answer'
+import MQuestion from '~/src/model/question'
 
-class FetchAuthor extends Base {
+class FetchQuestion extends Base {
   static get signature() {
     return `
         Fetch:Question
 
         {id:[必传]问题id}
-        `;
+        `
   }
 
   static get description() {
-    return "抓取知乎问题";
+    return '抓取知乎问题'
   }
 
   async execute(args: any, options: any): Promise<any> {
-    const { id: questionId } = args;
-    this.log(`开始抓取问题${questionId}下的数据`);
-    this.log(`获取用户信息`);
-    const authorInfo = await AuthorApi.asyncGetAutherInfo(urlToken);
-    await MAuthor.asyncReplaceAuthor(authorInfo);
-    this.log(`用户信息获取完毕`);
-    const name = authorInfo.name;
-    const answerCount = authorInfo.answer_count;
-    this.log(`用户${name}(${urlToken})共有${answerCount}个回答`);
-    this.log(`开始抓取回答列表`);
+    const { id: questionId } = args
+    this.log(`开始抓取问题${questionId}下的数据`)
+    this.log(`获取问题信息`)
+    const questionInfo = await QuestionApi.asyncGetQuestionInfo(questionId)
+    await MQuestion.asyncReplaceQuestionInfo(questionInfo)
+    this.log(`用户信息获取完毕`)
+    const title = questionInfo.title
+    const answerCount = questionInfo.answer_count
+    this.log(`问题${title}(${questionId})下共有${answerCount}个回答`)
+    this.log(`开始抓取回答列表`)
     for (let offset = 0; offset < answerCount; offset = offset + this.max) {
-      let answerList = await AnswerApi.asyncGetAutherAnswerList(
-        urlToken,
-        offset,
-        this.max
-      );
+      let answerList = await QuestionApi.asyncGetAnswerList(questionId, offset, this.max)
       for (let answer of answerList) {
-        await MAnswer.asyncReplaceAnswer(answer);
+        await MQuestion.asyncReplaceQuestionAnswer(questionId, answer)
       }
-      this.log(`第${offset}~${offset + this.max}条回答抓取完毕`);
+      this.log(`第${offset}~${offset + this.max}条回答抓取完毕`)
     }
-    this.log(`全部回答抓取完毕`);
+    this.log(`全部回答抓取完毕`)
   }
 }
 
-export default FetchAuthor;
+export default FetchQuestion
