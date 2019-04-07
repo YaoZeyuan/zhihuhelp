c21105fd19282bb0e204e3789502d1b58077a34d
yaozeyuan <yaozeyuan001@lianjia.com>
2019-04-08 00:36:16 +0800

    feat: 添加用户所有提问api

--- src/api/answer.ts
+++ src/api/answer.ts
@@ -1,9 +1,11 @@
 import _ from 'lodash'
 import Base from '~/src/api/base'
 import TypeAnswer from '~/src/type/namespace/answer'
+import AuthorApi from '~/src/api/author'
 
 class Answer extends Base {
   /**
+   * @deprecated 已废弃, 迁移至Author中
    * 获取用户回答列表
    * @param url_token
    * @param offset
@@ -11,19 +13,7 @@ class Answer extends Base {
    * @param sortBy
    */
   static async asyncGetAutherAnswerList(url_token: string, offset: number = 0, limit: number = 20, sortBy: string = Base.CONST_SORT_BY_CREATED): Promise<Array<TypeAnswer.Record>> {
-    const baseUrl = `https://www.zhihu.com/api/v4/members/${url_token}/answers`
-    const config = {
-      include:
-        'data[*].is_normal,admin_closed_comment,reward_info,is_collapsed,annotation_action,annotation_detail,collapse_reason,collapsed_by,suggest_edit,comment_count,can_comment,content,voteup_count,reshipment_settings,comment_permission,mark_infos,created_time,updated_time,review_info,question,excerpt,is_labeled,label_info,relationship.is_authorized,voting,is_author,is_thanked,is_nothelp;data[*].author.badge[?(type=best_answerer)].topics',
-      offset: offset,
-      limit: limit,
-      sort_by: sortBy,
-    }
-    const record = await Base.http.get(baseUrl, {
-      params: config,
-    })
-    const answerList = _.get(record, ['data'], [])
-    return answerList
+    return AuthorApi.asyncGetAutherAnswerList(url_token, offset, limit, sortBy)
   }
 
   /**
--- src/api/author.ts
+++ src/api/author.ts
@@ -1,6 +1,7 @@
 import _ from 'lodash'
 import Base from '~/src/api/base'
 import TypeAuthor from '~/src/type/namespace/author'
+import TypeAnswer from '~/src/type/namespace/answer'
 
 class Author extends Base {
   /**
@@ -14,10 +15,52 @@ class Author extends Base {
     const baseUrl = `https://api.zhihu.com/people/${account}`
     const config = {}
     const record: TypeAuthor.Record = await Base.http.get(baseUrl, {
-      params: config
+      params: config,
     })
     const info = record
     return info
+  }
+
+  /**
+   * 获取用户回答列表
+   * @param url_token
+   * @param offset
+   * @param limit
+   * @param sortBy
+   */
+  static async asyncGetAutherAnswerList(url_token: string, offset: number = 0, limit: number = 20, sortBy: string = Base.CONST_SORT_BY_CREATED): Promise<Array<TypeAnswer.Record>> {
+    const baseUrl = `https://www.zhihu.com/api/v4/members/${url_token}/answers`
+    const config = {
+      include:
+        'data[*].is_normal,admin_closed_comment,reward_info,is_collapsed,annotation_action,annotation_detail,collapse_reason,collapsed_by,suggest_edit,comment_count,can_comment,content,voteup_count,reshipment_settings,comment_permission,mark_infos,created_time,updated_time,review_info,question,excerpt,is_labeled,label_info,relationship.is_authorized,voting,is_author,is_thanked,is_nothelp;data[*].author.badge[?(type=best_answerer)].topics',
+      offset: offset,
+      limit: limit,
+      sort_by: sortBy,
+    }
+    const record = await Base.http.get(baseUrl, {
+      params: config,
+    })
+    const answerList = _.get(record, ['data'], [])
+    return answerList
+  }
+
+  /**
+   * 获取用户提问列表
+   * @param url_token
+   * @param offset
+   * @param limit
+   */
+  static async asyncGetAutherQuestionList(url_token: string, offset: number = 0, limit: number = 20): Promise<Array<TypeAnswer.Record>> {
+    const baseUrl = `https://www.zhihu.com/api/v4/members/${url_token}/questions`
+    const config = {
+      offset: offset,
+      limit: limit,
+    }
+    const record = await Base.http.get(baseUrl, {
+      params: config,
+    })
+    const answerList = _.get(record, ['data'], [])
+    return answerList
   }
 }
 export default Author
