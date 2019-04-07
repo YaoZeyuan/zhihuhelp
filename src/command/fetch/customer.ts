114b9ddf8a9de76f846881b9ac3784cd8f83277e
yaozeyuan <yaozeyuan001@lianjia.com>
2019-04-07 19:09:39 +0800

    feat: 初始化任务类型

---
+++ src/command/fetch/customer.ts
@@ -0,0 +1,65 @@
+import Base from '~/src/command/fetch/base'
+import ColumnApi from '~/src/api/column'
+import ArticleApi from '~/src/api/article'
+import MColumn from '~/src/model/column'
+import MArticle from '~/src/model/article'
+import CommonUtil from '~/src/library/util/common'
+import TypeArticleExcerpt from '~/src/type/namespace/article_excerpt'
+import TypeTaskConfig from '~/src/type/namespace/task_config'
+
+class FetchAuthor extends Base {
+  static get signature() {
+    return `
+        Fetch:Customer
+        {fetchConfigJSON:[必填]json形式的抓取配置}
+    `
+  }
+
+  static get description() {
+    return '自定义抓取任务'
+  }
+
+  async execute(args: any, options: any): Promise<any> {
+    let { fetchConfigJSON } = args
+    let customerTaskConfig: TypeTaskConfig.Customer = JSON.parse(fetchConfigJSON)
+    this.log(`开始进行自定义抓取, 共有${customerTaskConfig.config_list.length}个任务`)
+    for (let taskConfig of customerTaskConfig.config_list) {
+      switch (taskConfig.type) {
+        case 'author-ask-question':
+          break
+        case 'author-answer':
+          break
+        case 'author-pin':
+          break
+        case 'topic':
+          break
+        case 'collection':
+          break
+        case 'column':
+          break
+        case 'article':
+          break
+        case 'question':
+          break
+        case 'answer':
+          break
+        case 'pin':
+          break
+        case 'author-activity':
+          break
+        case 'author-agree':
+          break
+        case 'author-agree-article':
+          break
+        case 'author-agree-answer':
+          break
+        case 'author-watch-question':
+          break
+        default:
+          this.log(`不支持的任务类型:${taskConfig.type}, 自动跳过`)
+      }
+    }
+  }
+}
+
+export default FetchAuthor
--- src/type/namespace/task_config.d.ts
+++ src/type/namespace/task_config.d.ts
@@ -1,27 +1,29 @@
 declare namespace TaskConfig {
-    type AuthorCollectionType = "author-question" | "author-answer" | "author-pin" | "author-agree" | "author-watch-question" | "author-activity"
-    type ItemCollectionType = "topic" | "collection" | "column" | "article" | "question" | "answer" | "pin"
-    type OrderBy = "default" | "createAt" | "updateAt" | "voteUpCount" | "commentCount"
-    type ImageQuilty = "default" | "none" | "raw" | "hd"
-    type Record = {
-        "type": ItemCollectionType | AuthorCollectionType,
-        "id": string | number,
-        "orderBy": OrderBy,
-        "order": "asc" | "desc",
-        "imageQuilty": ImageQuilty, // 图片质量
-        "coverImage": string, // 封面图, 默认为王闹海
-        "comment": string // 备注
-    }
+  type authorQuestion = 'author-question'
 
-    // 自定义抓取
-    type Customer = {
-        "type": "customer",
-        "config_list": Array<Record>,
-        "imageQuilty": ImageQuilty, // 图片质量
-        "coverImage": string, // 封面图, 默认为王闹海
-        "title": string, // 书名
-        "comment": string // 备注
-    }
+  type AuthorCollectionType = 'author-ask-question' | 'author-answer' | 'author-pin' | 'author-activity' | 'author-agree' | 'author-agree-answer' | 'author-agree-article' | 'author-watch-question'
+  type ItemCollectionType = 'topic' | 'collection' | 'column' | 'article' | 'question' | 'answer' | 'pin'
+  type OrderBy = 'default' | 'createAt' | 'updateAt' | 'voteUpCount' | 'commentCount'
+  type ImageQuilty = 'default' | 'none' | 'raw' | 'hd'
+  type Record = {
+    type: ItemCollectionType | AuthorCollectionType
+    id: string | number
+    orderBy: OrderBy
+    order: 'asc' | 'desc'
+    imageQuilty: ImageQuilty // 图片质量
+    coverImage: string // 封面图, 默认为王闹海
+    comment: string // 备注
+  }
+
+  // 自定义抓取
+  type Customer = {
+    type: 'customer'
+    config_list: Array<Record>
+    imageQuilty: ImageQuilty // 图片质量
+    coverImage: string // 封面图, 默认为王闹海
+    title: string // 书名
+    comment: string // 备注
+  }
 }
 
 export default TaskConfig
