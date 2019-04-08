2a414a08033e28b3af008e65eeb241dedd5c3ae3
yaozeyuan <yaozeyuan001@lianjia.com>
2019-04-08 22:28:58 +0800

    feat: 想法&用户想法抓取部分完成

---
+++ src/type/namespace/pin.ts
@@ -0,0 +1,88 @@
+declare namespace Pin {
+  type Record = {
+    virtuals: {
+      is_liked: false
+      is_favorited: false
+    }
+    updated: 1554523087
+    repin_count: 2
+    likers: []
+    tags: []
+    admin_closed_comment: false
+    meet_reaction_guide_conditions: true
+    like_count: 0
+    top_reactions: {
+      like: 61
+    }
+    id: '1097830077735231488'
+    reaction_count: 61
+    view_permission: 'all'
+    is_deleted: false
+    author: {
+      is_followed: false
+      is_blocking: false
+      name: '丧心病狂刘老湿'
+      headline: '会讲点儿故事，略懂点儿历史，稍有点儿意思。'
+      gender: 1
+      user_type: 'people'
+      id: 'dd2a620fbc04bf35f833006745a46e84'
+      vip_info: {
+        widget: {
+          night_mode_url: 'https://pic3.zhimg.com/50/v2-6f3b7cd3f2422c39d07216685d7bee6c_r.png'
+          url: 'https://pic1.zhimg.com/50/v2-26f3fde381472751735f607fcd947da3_r.png'
+          id: 12001
+        }
+        is_vip: true
+        vip_icon: {
+          night_mode_url: 'https://pic3.zhimg.com/50/v2-c9686ff064ea3579730756ac6c289978_l.png'
+          url: 'https://pic2.zhimg.com/50/v2-034fefe15c83232cb448718998719d34_l.png'
+        }
+      }
+      exposed_medal: {
+        medal_name: '专业认可'
+        medal_id: '1055464808517865473'
+        description: '回答收到「专业认可」即可获得'
+        mini_avatar_url: 'https://pic4.zhimg.com/50/v2-ac316eb2c367f9a27136f46ab04f4f6a_b.jpg'
+        avatar_url: 'https://pic3.zhimg.com/50/v2-07aa4ae62dd57f3b172771c3d0bb6238_b.jpg'
+      }
+      avatar_url: 'https://pic2.zhimg.com/50/d6dc9e33e_l.jpg'
+      is_following: true
+      url: '/people/dd2a620fbc04bf35f833006745a46e84'
+      follower_count: 111779
+      is_blocked: false
+      type: 'people'
+      url_token: 'bingo-liu-66'
+      reaction_count: 30039
+    }
+    url: '/pins/1097830077735231488'
+    source_pin_id: 0
+    created: 1554523087
+    comment_count: 22
+    comments: []
+    content: [
+      {
+        content: '哈尔滨沙赞基本没人看……这可是假期啊'
+        fold_type: 'raw'
+        own_text: '哈尔滨沙赞基本没人看……这可是假期啊'
+        type: 'text'
+      },
+      {
+        url: 'https://pic2.zhimg.com/50/v2-e42ce349576557e5a739d2621d518679_qhd.jpg'
+        thumbnail: ''
+        width: 4096
+        height: 3072
+        type: 'image'
+        is_gif: false
+      }
+    ]
+    state: 'normal'
+    tag_specials: {}
+    excerpt_title: '哈尔滨沙赞基本没人看……这可是假期啊'
+    comment_permission: 'all'
+    type: 'pin'
+    content_html: '<div>哈尔滨沙赞基本没人看……这可是假期啊</div><img src="https://pic2.zhimg.com/50/v2-e42ce349576557e5a739d2621d518679_b.jpg" data-rawwidth="4096" data-rawheight="3072" class="origin_image zh-lightbox-thumb" width="4096" data-original="https://pic2.zhimg.com/50/v2-e42ce349576557e5a739d2621d518679_r.jpg">'
+    is_admin_close_repin: false
+  }
+}
+
+export default Pin
