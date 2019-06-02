b9600cc43a211eaf730eaad6a3f5e5eee37a7e70
yaozeyuan <yaozeyuan001@lianjia.com>
2019-06-02 23:38:59 +0800

    feat: 更新自定义任务配置类型声明

--- src/type/namespace/task_config.d.ts
+++ src/type/namespace/task_config.d.ts
@@ -1,27 +1,29 @@
 declare namespace TaskConfig {
   type authorQuestion = 'author-question'
 
-  type AuthorCollectionType = 'author-ask-question' | 'author-answer' | 'author-pin' | 'author-agree-answer' | 'author-agree-article' | 'author-watch-question'
+  type AuthorCollectionType =
+    | 'author-ask-question'
+    | 'author-answer'
+    | 'author-pin'
+    | 'author-agree-answer'
+    | 'author-agree-article'
+    | 'author-watch-question'
   type ItemCollectionType = 'topic' | 'collection' | 'column' | 'article' | 'question' | 'answer' | 'pin'
   type OrderBy = 'default' | 'createAt' | 'updateAt' | 'voteUpCount' | 'commentCount'
   type ImageQuilty = 'default' | 'none' | 'raw' | 'hd'
   type Record = {
     type: ItemCollectionType | AuthorCollectionType
     id: string | number
-    orderBy: OrderBy
-    order: 'asc' | 'desc'
-    imageQuilty: ImageQuilty // 图片质量
-    coverImage: string // 封面图, 默认为王闹海
+    rawInputText: string
     comment: string // 备注
   }
 
   // 自定义抓取
   type Customer = {
-    type: 'customer'
-    config_list: Array<Record>
+    configList: Array<Record>
     imageQuilty: ImageQuilty // 图片质量
     coverImage: string // 封面图, 默认为王闹海
-    bookname: string // 书名
+    bookTitle: string // 书名
     comment: string // 备注
     orderBy: OrderBy
     order: 'asc' | 'desc'
