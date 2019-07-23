19307b10fb2acf4ae687dc13e09e4eb9b248e178
yaozeyuan <yaozeyan93@gmail.com>
2019-07-23 22:05:43 +0800

    fix: 取用户最后活跃时间的代码写错了

--- src/api/activity.ts
+++ src/api/activity.ts
@@ -5,14 +5,18 @@ import moment from 'moment'
 
 class Activity extends Base {
   /**
-   * 获取用户活动列表 
+   * 获取用户活动列表
    * https://www.zhihu.com/api/v4/members/404-Page-Not-found/activities?limit=10&after_id=1547034952&desktop=True
    * @param url_token
    * @param afterTimeAt 从X时间后
    * @param limit
    * @param sortBy
    */
-  static async asyncGetAutherActivityList(url_token: string, afterTimeAt: number = 0, limit: number = 20): Promise<Array<TypeActivity.Record>> {
+  static async asyncGetAutherActivityList(
+    url_token: string,
+    afterTimeAt: number = 0,
+    limit: number = 20,
+  ): Promise<Array<TypeActivity.Record>> {
     const baseUrl = `https://www.zhihu.com/api/v4/members/${url_token}/activities`
     const config = {
       after_id: afterTimeAt,
@@ -20,17 +24,17 @@ class Activity extends Base {
       desktop: 'True',
     }
     const record = await Base.http.get(baseUrl, {
-      params: config
+      params: config,
     })
     const activityList = _.get(record, ['data'], [])
     return activityList
   }
 
   /**
-  * 检查指定时间后是否还有用户活跃记录 
-  * @param url_token
-  * @param afterTimeAt 从X时间后
-  */
+   * 检查指定时间后是否还有用户活跃记录
+   * @param url_token
+   * @param afterTimeAt 从X时间后
+   */
   static async asyncCheckHasAutherActivityAfterAt(url_token: string, afterTimeAt: number = 0): Promise<boolean> {
     const baseUrl = `https://www.zhihu.com/api/v4/members/${url_token}/activities`
     const config = {
@@ -39,16 +43,16 @@ class Activity extends Base {
       desktop: 'True',
     }
     const record = await Base.http.get(baseUrl, {
-      params: config
+      params: config,
     })
     const hasActivity = _.get(record, ['paging', 'is_end'], false)
     return hasActivity === false
   }
 
   /**
-  * 获取用户最近一次活跃时间 
-  * @param url_token
-  */
+   * 获取用户最近一次活跃时间
+   * @param url_token
+   */
   static async asyncGetAutherLastActivityAt(url_token: string): Promise<number> {
     const baseUrl = `https://www.zhihu.com/api/v4/members/${url_token}/activities`
     let now = moment().unix()
@@ -58,9 +62,13 @@ class Activity extends Base {
       desktop: 'True',
     }
     const record = await Base.http.get(baseUrl, {
-      params: config
+      params: config,
     })
-    const lastActivityAt = _.get(record, ['data', 0, 'id'], now)
+    let lastActivityMsAt = _.get(record, ['data', 0, 'id'], 0)
+    let lastActivityAt = lastActivityMsAt / 1000 // 取到的id是毫秒值, 因此需要除以1000
+    if (lastActivityAt <= 0) {
+      lastActivityAt = now
+    }
     return lastActivityAt
   }
 }
