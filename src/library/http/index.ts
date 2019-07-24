dd295d7958afdf29d7c3d1b51a9f532d097de15e
yaozeyuan <yaozeyan93@gmail.com>
2019-07-24 21:28:25 +0800

    feat: 捕捉网络请求错误, 方便及时监控到知乎的屏蔽消息

--- src/library/http/index.ts
+++ src/library/http/index.ts
@@ -6,42 +6,43 @@ import _ from 'lodash'
 
 // 创建axios实例
 const http = axios.create({
-    timeout: RequestConfig.timeoutMs,
-    headers: {
-        // 加上ua
-        'User-Agent': RequestConfig.ua,
-        cookie: RequestConfig.cookie,
-    }
+  timeout: RequestConfig.timeoutMs,
+  headers: {
+    // 加上ua
+    'User-Agent': RequestConfig.ua,
+    cookie: RequestConfig.cookie,
+  },
 })
 
 class Http {
-    /**
-     * 封装get方法
-     * @param url
-     * @param config
-     */
-    static async get(url: string, config: AxiosRequestConfig = {}) {
-        const response = await http.get(url, config).catch(() => {
-            return {}
-        })
-        const record = _.get(response, ['data'], {})
-        return record
-    }
+  /**
+   * 封装get方法
+   * @param url
+   * @param config
+   */
+  static async get(url: string, config: AxiosRequestConfig = {}) {
+    const response = await http.get(url, config).catch(e => {
+      logger.log(`网络请求失败, 错误内容=> message:${e.message}, stack=>${e.stack}`)
+      return {}
+    })
+    const record = _.get(response, ['data'], {})
+    return record
+  }
 
-    /**
-     * axios封装的arraybuffer由于使用了stream, 重复次数多了之后会出现stream卡死的情况, 且不可恢复
-     * 因此改用request封装图片下载请求
-     * @param url 
-     */
-    static async downloadImg(url: string): Promise<request.RequestPromise> {
-        return await request({
-            url,
-            method: 'get',
-            // 数据以二进制形式返回
-            encoding: null,
-            timeout: RequestConfig.timeoutMs,
-        })
-    }
+  /**
+   * axios封装的arraybuffer由于使用了stream, 重复次数多了之后会出现stream卡死的情况, 且不可恢复
+   * 因此改用request封装图片下载请求
+   * @param url
+   */
+  static async downloadImg(url: string): Promise<request.RequestPromise> {
+    return await request({
+      url,
+      method: 'get',
+      // 数据以二进制形式返回
+      encoding: null,
+      timeout: RequestConfig.timeoutMs,
+    })
+  }
 }
 
 export default Http
