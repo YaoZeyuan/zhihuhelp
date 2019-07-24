3129c9cff89158beade735c358e73f7a32c873ad
yaozeyuan <yaozeyan93@gmail.com>
2019-07-25 00:36:31 +0800

    feat: 修改派发任务命令, 调整为自定义模式

--- src/command/dispatch_task.ts
+++ src/command/dispatch_task.ts
@@ -9,73 +9,38 @@ import shelljs from 'shelljs'
 import DatabaseConfig from '~/src/config/database'
 import PathConfig from '~/src/config/path'
 import InitEnvCommand from '~/src/command/init_env'
-import FetchActivityCommand from '~/src/command/fetch/activity'
-import FetchAuthorCommand from '~/src/command/fetch/author'
-import FetchCollectionCommand from '~/src/command/fetch/collection'
-import FetchColumnCommand from '~/src/command/fetch/column'
-import FetchTopicCommand from '~/src/command/fetch/topic'
+import FetchCustomerCommand from '~/src/command/fetch/customer'
 
-import GenerateActivityCommand from '~/src/command/generate/activity'
-import GenerateAuthorCommand from '~/src/command/generate/author'
-import GenerateCollectionCommand from '~/src/command/generate/collection'
-import GenerateColumnCommand from '~/src/command/generate/column'
-import GenerateTopicCommand from '~/src/command/generate/topic'
+import GenerateCustomerCommand from '~/src/command/generate/customer'
 
 class DispatchCommand extends Base {
-  static get signature () {
+  static get signature() {
     return `
       Dispatch:Task
      `
   }
 
-  static get description () {
+  static get description() {
     return '根据task_config_list.json配置, 分发任务'
   }
 
-  async execute (args: any, options: any) {
+  async execute(args: any, options: any) {
     this.log(`检查更新`)
     let taskConfigListJson = fs.readFileSync(PathConfig.customerTaskConfigUri)
     let taskConfigList: Array<TypeTaskConfig.Record> = JSON.parse(taskConfigListJson.toString())
     // 初始化运行环境
     let initCommand = new InitEnvCommand()
     await initCommand.handle({}, {})
-    let fetchCommand
-    let generateCommand
-    for (let taskConfig of taskConfigList) {
-      switch (taskConfig.type) {
-        case 'activity':
-          fetchCommand = new FetchActivityCommand()
-          await fetchCommand.handle({ account: taskConfig.id }, {})
-          generateCommand = new GenerateActivityCommand()
-          await generateCommand.handle({ account: taskConfig.id }, {})
-          break
-        case 'author':
-          fetchCommand = new FetchAuthorCommand()
-          await fetchCommand.handle({ account: taskConfig.id }, {})
-          generateCommand = new GenerateAuthorCommand()
-          await generateCommand.handle({ account: taskConfig.id }, {})
-          break
-        case 'collection':
-          fetchCommand = new FetchCollectionCommand()
-          await fetchCommand.handle({ collectionId: taskConfig.id }, {})
-          generateCommand = new GenerateCollectionCommand()
-          await generateCommand.handle({ collectionId: taskConfig.id }, {})
-          break
-        case 'column':
-          fetchCommand = new FetchColumnCommand()
-          await fetchCommand.handle({ columnId: taskConfig.id }, {})
-          generateCommand = new GenerateColumnCommand()
-          await generateCommand.handle({ columnId: taskConfig.id }, {})
-          break
-        case 'topic':
-          fetchCommand = new FetchTopicCommand()
-          await fetchCommand.handle({ topicId: taskConfig.id }, {})
-          generateCommand = new GenerateTopicCommand()
-          await generateCommand.handle({ topicId: taskConfig.id }, {})
-          break
-      }
-    }
-    this.log(`执行完毕`)
+    this.log(`创建任务实例`)
+    let fetchCommand = new FetchCustomerCommand()
+    let generateCommand = new GenerateCustomerCommand()
+    this.log(`执行抓取命令`)
+    await fetchCommand.handle({}, {})
+    this.log(`抓取命令执行完毕`)
+    this.log(`执行生成电子书命令`)
+    await generateCommand.handle({}, {})
+    this.log(`生成电子书命令执行完毕`)
+    this.log(`所有命令执行完毕`)
   }
 }
 
