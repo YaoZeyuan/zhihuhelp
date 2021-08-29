ed8e55668e85c0f9c1bee68bd54daae1bd761ecb
yaozeyuan <yaozeyuan93@gmail.com>
2021-08-29 23:08:14 +0800

    feat: 初始化vite-electron项目

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './view/App.vue'
 
-createApp(App).mount('#app')
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+let app = createApp(App)
+
+app.use(ElementPlus)
+app.mount('#app')
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-// https://vitejs.dev/config/
+// https://cn.vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+  ],
+  server: {
+    port: 8080,
+    fs: {
+      strict: false,
+    }
+  },
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        format: 'cjs', // 配置 Rollup 打包输出 CommonJs 格式
+      },
+      external: ['electron'], // 告诉 Rollup 不要去打包 electron
+    },
+  },
+  optimizeDeps: {
+    exclude: ['electron'], // 告诉 Vite 不要转换 electron 模块
+  },
+  resolve: {
+    alias: [
+      {
+        "find": "~/client",
+        "replacement": path.resolve(__dirname)
+      }
+    ]
+  },
+
 })
