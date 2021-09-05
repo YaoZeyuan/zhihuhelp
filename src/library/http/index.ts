ff102b28ed8b6e7552ee154d18c49217ea9470e6
yaozeyuan <yaozeyuan93@gmail.com>
2021-09-05 18:45:24 +0800

    feat: 支持在请求知乎接口时, 进行加密

--- src/library/http/index.ts
+++ src/library/http/index.ts
@@ -1,8 +1,13 @@
 import axios, { AxiosRequestConfig } from 'axios'
 import RequestConfig from '~/src/config/request'
 import logger from '~/src/library/logger'
+import getZhihuEncrypt from '~/src/library/zhihu_encrypt/index'
 import request from 'request-promise'
 import _ from 'lodash'
+import URL from 'url'
+
+const Const_Headers_x_zse_93 = '101_3_2.0'
+
 
 // 创建axios实例
 const http = axios.create({
@@ -21,6 +26,24 @@ class Http {
    * @param config
    */
   static async get(url: string, config: AxiosRequestConfig = {}) {
+    // 发送知乎请求时, 需要额外附带校验header, 否则报错
+
+    let cookie_item_list = RequestConfig.cookie.split(';').map(item => item.trim()).filter(item => item.startsWith('d_c0'))
+    let raw_d_c0 = cookie_item_list?.[0] || ''
+    let cookie_d_c0 = raw_d_c0.split("d_c0=")?.[1] || ''
+
+    let url_obj = new URL.URL(url)
+    let encrypt_url = `${url_obj.pathname}?${url_obj.search}`;
+    let x_zst_81 = getZhihuEncrypt({
+      cookie_d_c0: cookie_d_c0,
+      "url": encrypt_url,
+    })
+    config.headers = {
+      ...config.headers,
+      'x-zse-93': Const_Headers_x_zse_93,
+      'x-zst-81': x_zst_81,
+    }
+
     const response = await http.get(url, config).catch(e => {
       logger.log(`网络请求失败, 您的账号可能因抓取频繁被知乎认为有风险, 在浏览器中访问知乎首页,输入验证码即可恢复`)
       logger.log(`错误内容=> message:${e.message}, stack=>${e.stack}`)
