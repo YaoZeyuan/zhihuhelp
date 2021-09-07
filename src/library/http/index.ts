f6a752f8714099498dfec884f5ca1b79b571800f
yaozeyuan <yaozeyuan93@gmail.com>
2021-09-07 23:18:46 +0800

    feat: 解决无法抓取知乎内容的问题

--- src/library/http/index.ts
+++ src/library/http/index.ts
@@ -3,6 +3,7 @@ import RequestConfig from '~/src/config/request'
 import logger from '~/src/library/logger'
 import getZhihuEncrypt from '~/src/library/zhihu_encrypt/index'
 import request from 'request-promise'
+import querystring from 'querystring'
 import _ from 'lodash'
 import URL from 'url'
 
@@ -19,33 +20,50 @@ const http = axios.create({
   },
 })
 
+function fixedEncodeURIComponent(str: string) {
+  // 不需要对*进行转码
+  return encodeURIComponent(str).replace(/[!'()]/g, function (c) {
+    return '%' + c.charCodeAt(0).toString(16);
+  });
+}
+
 class Http {
   /**
    * 封装get方法
-   * @param url
+   * @param rawUrl
    * @param config
    */
-  static async get(url: string, config: AxiosRequestConfig = {}) {
+  static async get(rawUrl: string, config: AxiosRequestConfig = {}) {
     // 发送知乎请求时, 需要额外附带校验header, 否则报错
 
     let cookie_item_list = RequestConfig.cookie.split(';').map(item => item.trim()).filter(item => item.startsWith('d_c0'))
     let raw_d_c0 = cookie_item_list?.[0] || ''
     let cookie_d_c0 = raw_d_c0.split("d_c0=")?.[1] || ''
 
+    let url = rawUrl
+    if (config.params) {
+      // 将config中的参数合并到url中, 以进行统一的签名运算
+      url = `${url}?${querystring.stringify(config.params, undefined, undefined, {
+        encodeURIComponent: fixedEncodeURIComponent
+      })}`
+      delete config.params
+    }
+
     let url_obj = new URL.URL(url)
-    let encrypt_url = `${url_obj.pathname}?${url_obj.search}`;
-    let x_zst_81 = getZhihuEncrypt({
+
+    let encrypt_url = `${url_obj.pathname}${url_obj.search}`;
+    let x_zst_96 = getZhihuEncrypt({
       cookie_d_c0: cookie_d_c0,
       "url": encrypt_url,
     })
     config.headers = {
       ...config.headers,
       'x-zse-93': Const_Headers_x_zse_93,
-      'x-zst-81': x_zst_81,
+      'x-zse-96': x_zst_96,
     }
 
     const response = await http.get(url, config).catch(e => {
-      logger.log(`网络请求失败, 您的账号可能因抓取频繁被知乎认为有风险, 在浏览器中访问知乎首页,输入验证码即可恢复`)
+      logger.log(`网络请求失败, 两种可能: 1.知乎更换了接口签名算法, 知乎私信@姚泽源 更新代码 2. 您的账号可能因抓取频繁被知乎认为有风险, 在浏览器中访问知乎首页,输入验证码即可恢复`)
       logger.log(`错误内容=> message:${e.message}, stack=>${e.stack}`)
       return {}
     })
