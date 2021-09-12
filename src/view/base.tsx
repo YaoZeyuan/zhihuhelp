07dfab05845bbbaea00c258418dbfa91db3f336f
yaozeyuan <yaozeyuan93@gmail.com>
2021-09-13 02:06:53 +0800

    feat: 添加版权声明

--- src/view/base.tsx
+++ src/view/base.tsx
@@ -110,10 +110,17 @@ class Base {
     }
 
     const indexTableElement = (
-      <div className="panel panel-success center-block">
-        <div className="panel-heading">{bookname}</div>
-        <div className="list-group">
-          <ol>{indexList}</ol>
+      <div>
+        <div className="panel panel-default">
+          <div className="panel-heading">电子书由知乎助手生成, 仅供个人阅读学习使用, 严禁用于商业用途</div>
+          <div className="panel-body">项目地址: https://www.yaozeyuan.online/zhihuhelp</div>
+        </div>
+        <p>&nbsp;</p>
+        <div className="panel panel-success center-block">
+          <div className="panel-heading">{bookname}</div>
+          <div className="list-group">
+            <ol>{indexList}</ol>
+          </div>
         </div>
       </div>
     )
