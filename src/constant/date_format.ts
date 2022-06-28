258fc5e554cf166745f75262a4a57b33d5fd8862
yaozeyuan001 <yaozeyuan001@ke.com>
2022-06-28 23:43:05 +0800

    feat: 优化常量

--- src/constant/date_format.ts
+++ src/constant/date_format.ts
@@ -1,213 +1,84 @@
-// 统一时间记录格式
-class DATE_FORMAT {
-  static get UNIT(): {
-    YEAR: 'year'
-    MONTH: 'month'
-    WEEK: 'week'
-    DAY: 'day'
-    HOUR: 'hour'
-    MINUTE: 'minute'
-    SECOND: 'second'
-    MILLSECOND: 'millsecond'
-  } {
-    return {
-      YEAR: 'year',
-      MONTH: 'month',
-      WEEK: 'week',
-      DAY: 'day',
-      HOUR: 'hour',
-      MINUTE: 'minute',
-      SECOND: 'second',
-      MILLSECOND: 'millsecond',
-    }
-  }
+// 标准单位
+export const Const_Unit_Year = 'year' as const
+export const Const_Unit_Month = 'month' as const
+export const Const_Unit_Day = 'day' as const
+export const Const_Unit_Hour = 'hour' as const
+export const Const_Unit_Minute = 'minute' as const
+export const Const_Unit_Second = 'second' as const
+export const Const_Unit_Millsecond = 'millsecond' as const
 
-  // 数据库GroupBy
-  static readonly SQL_GROUP_BY_YEAR: string = '%Y'
-  static readonly SQL_GROUP_BY_MONTH: string = '%Y-%m'
-  static readonly SQL_GROUP_BY_DAY: string = '%Y-%m-%d'
-  static readonly SQL_GROUP_BY_HOUR: string = '%Y-%m-%d %H'
-  static readonly SQL_GROUP_BY_MINUTE: string = '%Y-%m-%d %H:%i'
-  static readonly SQL_GROUP_BY_SECOND: string = '%Y-%m-%d %H:%i:%s'
-  static readonly SQL_GROUP_BY_MILLSECOND: string = '%Y-%m-%d %H:%i:%s.%f'
+// SQL相关
+export const Const_Sql_Group_By_Year = '%Y' as const
+export const Const_Sql_Group_By_Month = '%Y-%m' as const
+export const Const_Sql_Group_By_Day = '%Y-%m-%d' as const
+export const Const_Sql_Group_By_Hour = '%Y-%m-%d %H' as const
+export const Const_Sql_Group_By_Minute = '%Y-%m-%d %H:%i' as const
+export const Const_Sql_Group_By_Second = '%Y-%m-%d %H:%i:%s' as const
+export const Const_Sql_Group_By_Millsecond = '%Y-%m-%d %H:%i:%s.%f' as const
 
-  static get SQL_GROUP_BY_UNIT() {
-    return {
-      [DATE_FORMAT.UNIT.YEAR]: DATE_FORMAT.SQL_GROUP_BY_YEAR,
-      [DATE_FORMAT.UNIT.MONTH]: DATE_FORMAT.SQL_GROUP_BY_MONTH,
-      [DATE_FORMAT.UNIT.DAY]: DATE_FORMAT.SQL_GROUP_BY_DAY,
-      [DATE_FORMAT.UNIT.HOUR]: DATE_FORMAT.SQL_GROUP_BY_HOUR,
-      [DATE_FORMAT.UNIT.MINUTE]: DATE_FORMAT.SQL_GROUP_BY_MINUTE,
-      [DATE_FORMAT.UNIT.SECOND]: DATE_FORMAT.SQL_GROUP_BY_SECOND,
-      [DATE_FORMAT.UNIT.MILLSECOND]: DATE_FORMAT.SQL_GROUP_BY_MILLSECOND,
-    }
-  }
-
-  // 数据库记录
-  static get DATABASE_BY_YEAR() {
-    return 'YYYY'
-  }
-
-  static get DATABASE_BY_MONTH() {
-    return 'YYYY-MM'
-  }
-
-  static get DATABASE_BY_DAY() {
-    return 'YYYY-MM-DD'
-  }
-
-  static get DATABASE_BY_HOUR() {
-    return 'YYYY-MM-DD_HH'
-  }
-
-  static get DATABASE_BY_MINUTE() {
-    return 'YYYY-MM-DD_HH:mm'
-  }
-
-  static get DATABASE_BY_SECOND() {
-    return 'YYYY-MM-DD_HH:mm:ss'
-  }
-
-  static get DATABASE_BY_MILLSECOND() {
-    return 'YYYY-MM-DD_HH:mm:ss.SSS'
-  }
-
-  static get DATABASE_BY_UNIT() {
-    return {
-      [DATE_FORMAT.UNIT.YEAR]: DATE_FORMAT.DATABASE_BY_YEAR,
-      [DATE_FORMAT.UNIT.MONTH]: DATE_FORMAT.DATABASE_BY_MONTH,
-      [DATE_FORMAT.UNIT.DAY]: DATE_FORMAT.DATABASE_BY_DAY,
-      [DATE_FORMAT.UNIT.WEEK]: DATE_FORMAT.DATABASE_BY_DAY,
-      [DATE_FORMAT.UNIT.HOUR]: DATE_FORMAT.DATABASE_BY_HOUR,
-      [DATE_FORMAT.UNIT.MINUTE]: DATE_FORMAT.DATABASE_BY_MINUTE,
-      [DATE_FORMAT.UNIT.SECOND]: DATE_FORMAT.DATABASE_BY_SECOND,
-      [DATE_FORMAT.UNIT.MILLSECOND]: DATE_FORMAT.DATABASE_BY_MILLSECOND,
-    }
-  }
-
-  // ES记录
-  static get ELASTIC_SEARCH_BY_YEAR() {
-    return 'yyyy'
-  }
-
-  static get ELASTIC_SEARCH_BY_MONTH() {
-    return 'yyyy-MM'
-  }
-
-  static get ELASTIC_SEARCH_BY_DAY() {
-    return 'yyyy-MM-dd'
-  }
-
-  static get ELASTIC_SEARCH_BY_HOUR() {
-    return 'yyyy-MM-dd HH:00:00'
-  }
-
-  static get ELASTIC_SEARCH_BY_MINUTE() {
-    return 'yyyy-MM-dd HH:mm:00'
-  }
-
-  static get ELASTIC_SEARCH_BY_SECOND() {
-    return 'yyyy-MM-dd HH:mm:ss'
-  }
-
-  static get ELASTIC_SEARCH_BY_MILLSECOND() {
-    return 'yyyy-MM-dd HH:mm:ss.SSS'
-  }
-
-  static get ELASTIC_SEARCH_BY_UNIT() {
-    return {
-      [DATE_FORMAT.UNIT.YEAR]: DATE_FORMAT.ELASTIC_SEARCH_BY_YEAR,
-      [DATE_FORMAT.UNIT.MONTH]: DATE_FORMAT.ELASTIC_SEARCH_BY_MONTH,
-      [DATE_FORMAT.UNIT.DAY]: DATE_FORMAT.ELASTIC_SEARCH_BY_DAY,
-      [DATE_FORMAT.UNIT.HOUR]: DATE_FORMAT.ELASTIC_SEARCH_BY_HOUR,
-      [DATE_FORMAT.UNIT.MINUTE]: DATE_FORMAT.ELASTIC_SEARCH_BY_MINUTE,
-      [DATE_FORMAT.UNIT.SECOND]: DATE_FORMAT.ELASTIC_SEARCH_BY_SECOND,
-      [DATE_FORMAT.UNIT.MILLSECOND]: DATE_FORMAT.ELASTIC_SEARCH_BY_MILLSECOND,
-    }
-  }
-
-  // 命令行参数
-  static get COMMAND_ARGUMENT_BY_YEAR() {
-    return 'YYYY'
-  }
-
-  static get COMMAND_ARGUMENT_BY_MONTH() {
-    return 'YYYY-MM'
-  }
-
-  static get COMMAND_ARGUMENT_BY_DAY() {
-    return 'YYYY-MM-DD'
-  }
-
-  static get COMMAND_ARGUMENT_BY_HOUR() {
-    return 'YYYY-MM-DD HH'
-  }
-
-  static get COMMAND_ARGUMENT_BY_MINUTE() {
-    return 'YYYY-MM-DD HH:mm'
-  }
-
-  static get COMMAND_ARGUMENT_BY_SECOND() {
-    return 'YYYY-MM-DD HH:mm:ss'
-  }
-
-  static get COMMAND_ARGUMENT_BY_MILLSECOND() {
-    return 'YYYY-MM-DD HH:mm:ss.SSS'
-  }
-
-  static get COMMAND_ARGUMENT_BY_UNIT() {
-    return {
-      [DATE_FORMAT.UNIT.YEAR]: DATE_FORMAT.COMMAND_ARGUMENT_BY_YEAR,
-      [DATE_FORMAT.UNIT.MONTH]: DATE_FORMAT.COMMAND_ARGUMENT_BY_MONTH,
-      [DATE_FORMAT.UNIT.DAY]: DATE_FORMAT.COMMAND_ARGUMENT_BY_DAY,
-      [DATE_FORMAT.UNIT.WEEK]: DATE_FORMAT.COMMAND_ARGUMENT_BY_DAY,
-      [DATE_FORMAT.UNIT.HOUR]: DATE_FORMAT.COMMAND_ARGUMENT_BY_HOUR,
-      [DATE_FORMAT.UNIT.MINUTE]: DATE_FORMAT.COMMAND_ARGUMENT_BY_MINUTE,
-      [DATE_FORMAT.UNIT.SECOND]: DATE_FORMAT.COMMAND_ARGUMENT_BY_SECOND,
-      [DATE_FORMAT.UNIT.MILLSECOND]: DATE_FORMAT.COMMAND_ARGUMENT_BY_MILLSECOND,
-    }
-  }
-
-  // 数据展示
-  static get DISPLAY_BY_YEAR() {
-    return 'YYYY'
-  }
-
-  static get DISPLAY_BY_MONTH() {
-    return 'YYYY-MM'
-  }
-
-  static get DISPLAY_BY_DAY() {
-    return 'YYYY-MM-DD'
-  }
-
-  static get DISPLAY_BY_HOUR() {
-    return 'YYYY-MM-DD HH'
-  }
-
-  static get DISPLAY_BY_MINUTE() {
-    return 'YYYY-MM-DD HH:mm'
-  }
-
-  static get DISPLAY_BY_SECOND() {
-    return 'YYYY-MM-DD HH:mm:ss'
-  }
-
-  static get DISPLAY_BY_MILLSECOND() {
-    return 'YYYY-MM-DD HH:mm:ss.SSS'
-  }
-
-  static get DISPLAY_BY_UNIT() {
-    return {
-      [DATE_FORMAT.UNIT.YEAR]: DATE_FORMAT.DISPLAY_BY_YEAR,
-      [DATE_FORMAT.UNIT.MONTH]: DATE_FORMAT.DISPLAY_BY_MONTH,
-      [DATE_FORMAT.UNIT.DAY]: DATE_FORMAT.DISPLAY_BY_DAY,
-      [DATE_FORMAT.UNIT.HOUR]: DATE_FORMAT.DISPLAY_BY_HOUR,
-      [DATE_FORMAT.UNIT.MINUTE]: DATE_FORMAT.DISPLAY_BY_MINUTE,
-      [DATE_FORMAT.UNIT.SECOND]: DATE_FORMAT.DISPLAY_BY_SECOND,
-      [DATE_FORMAT.UNIT.MILLSECOND]: DATE_FORMAT.DISPLAY_BY_MILLSECOND,
-    }
-  }
+export const Const_Sql_By_Unit = {
+  [Const_Unit_Year]: Const_Sql_Group_By_Year,
+  [Const_Unit_Month]: Const_Sql_Group_By_Month,
+  [Const_Unit_Day]: Const_Sql_Group_By_Day,
+  [Const_Unit_Hour]: Const_Sql_Group_By_Hour,
+  [Const_Unit_Minute]: Const_Sql_Group_By_Minute,
+  [Const_Unit_Second]: Const_Sql_Group_By_Second,
+  [Const_Unit_Millsecond]: Const_Sql_Group_By_Millsecond,
 }
 
-export default DATE_FORMAT
+// 数据库相关
+export const Const_Database_By_Year = 'YYYY' as const
+export const Const_Database_By_Month = 'YYYY-MM' as const
+export const Const_Database_By_Day = 'YYYY-MM-DD' as const
+export const Const_Database_By_Hour = 'YYYY-MM-DD_HH' as const
+export const Const_Database_By_Minute = 'YYYY-MM-DD_HH:mm' as const
+export const Const_Database_By_Second = 'YYYY-MM-DD_HH:mm:ss' as const
+export const Const_Database_By_Millsecond = 'YYYY-MM-DD_HH:mm:ss.SSS' as const
+
+export const Const_Database_By_Unit = {
+  [Const_Unit_Year]: Const_Database_By_Year,
+  [Const_Unit_Month]: Const_Database_By_Month,
+  [Const_Unit_Day]: Const_Database_By_Day,
+  [Const_Unit_Hour]: Const_Database_By_Hour,
+  [Const_Unit_Minute]: Const_Database_By_Minute,
+  [Const_Unit_Second]: Const_Database_By_Second,
+  [Const_Unit_Millsecond]: Const_Database_By_Millsecond,
+}
+
+// 展示相关
+export const Const_Display_By_Year = 'YYYY' as const
+export const Const_Display_By_Month = 'YYYY-MM' as const
+export const Const_Display_By_Day = 'YYYY-MM-DD' as const
+export const Const_Display_By_Hour = 'YYYY-MM-DD HH' as const
+export const Const_Display_By_Minute = 'YYYY-MM-DD HH:mm' as const
+export const Const_Display_By_Second = 'YYYY-MM-DD HH:mm:ss' as const
+export const Const_Display_By_Millsecond = 'YYYY-MM-DD HH:mm:ss.SSS' as const
+
+export const Const_Display_By_Unit = {
+  [Const_Unit_Year]: Const_Display_By_Year,
+  [Const_Unit_Month]: Const_Display_By_Month,
+  [Const_Unit_Day]: Const_Display_By_Day,
+  [Const_Unit_Hour]: Const_Display_By_Hour,
+  [Const_Unit_Minute]: Const_Display_By_Minute,
+  [Const_Unit_Second]: Const_Display_By_Second,
+  [Const_Unit_Millsecond]: Const_Display_By_Millsecond,
+}
+
+// 命令行相关
+export const Const_Command_Argument_By_Year = 'YYYY' as const
+export const Const_Command_Argument_By_Month = 'YYYY-MM' as const
+export const Const_Command_Argument_By_Day = 'YYYY-MM-DD' as const
+export const Const_Command_Argument_By_Hour = 'YYYY-MM-DD HH' as const
+export const Const_Command_Argument_By_Minute = 'YYYY-MM-DD HH:mm' as const
+export const Const_Command_Argument_By_Second = 'YYYY-MM-DD HH:mm:ss' as const
+export const Const_Command_Argument_By_Millsecond = 'YYYY-MM-DD HH:mm:ss.SSS' as const
+
+export const Const_Command_Argument_By_Unit = {
+  [Const_Unit_Year]: Const_Command_Argument_By_Year,
+  [Const_Unit_Month]: Const_Command_Argument_By_Month,
+  [Const_Unit_Day]: Const_Command_Argument_By_Day,
+  [Const_Unit_Hour]: Const_Command_Argument_By_Hour,
+  [Const_Unit_Minute]: Const_Command_Argument_By_Minute,
+  [Const_Unit_Second]: Const_Command_Argument_By_Second,
+  [Const_Unit_Millsecond]: Const_Command_Argument_By_Millsecond,
+}
