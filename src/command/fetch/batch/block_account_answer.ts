d9307cf0841ac6268fe283dc4d41235bed6387e4
yaozeyuan001 <yaozeyuan001@ke.com>
2022-07-03 22:45:07 +0800

    feat: 继续进行接口任务中心改造

--- src/command/fetch/batch/block_account_answer.ts
+++ src/command/fetch/batch/block_account_answer.ts
@@ -1,6 +1,4 @@
 import AuthorApi from '~/src/api/author'
-import AnswerApi from '~/src/api/answer'
-import MAnswer from '~/src/model/answer'
 import MAuthor from '~/src/model/author'
 import Base from '~/src/command/fetch/batch/base'
 import BatchFetchAnswer from '~/src/command/fetch/batch/answer'
@@ -17,22 +15,24 @@ class BatchFetchAuthorAnswer extends Base {
     const answerCount = authorInfo.answer_count
     this.log(`用户${name}(${urlToken})共有${answerCount}个回答`)
     this.log(`开始抓取回答列表`)
-    let answetIdList = []
+    let answetIdList: string[] = []
     let batchFetchAnswer = new BatchFetchAnswer()
-    let loopCounter = 0
     for (let offset = 0; offset < answerCount; offset = offset + this.max) {
-      let answerList = await AnswerApi.asyncGetAutherAnswerList(urlToken, offset, this.max)
-      for (let answer of answerList) {
-        let answerId = `${answer.id}`
-        answetIdList.push(answerId)
+      let asyncTaskFunc = async () => {
+        let answerList = await AuthorApi.asyncGetAutherAnswerList(urlToken, offset, this.max)
+        for (let answer of answerList) {
+          let answerId = `${answer.id}`
+          answetIdList.push(answerId)
+        }
+        this.log(`第${offset}~${offset + this.max}条回答记录抓取完毕`)
       }
-      this.log(`第${offset}~${offset + this.max}条回答记录抓取完毕`)
-      loopCounter = loopCounter + 1
-      if (loopCounter % 10 === 0) {
-        this.log(`第${loopCounter}次抓取, 休眠1s, 保护知乎服务器`)
-        await CommonUtil.asyncSleep(1 * 1000)
-      }
+      CommonUtil.addAsyncTaskFunc({
+        asyncTaskFunc,
+        needProtect: true,
+        label: this,
+      })
     }
+    await CommonUtil.asyncWaitAllTaskCompleteByLabel(this)
     this.log(`开始抓取用户${name}(${urlToken})的所有回答记录,共${answetIdList.length}条`)
     await batchFetchAnswer.fetchListAndSaveToDb(answetIdList)
     this.log(`用户${name}(${urlToken})的回答记录抓取完毕`)
--- src/command/fetch/batch/collection.ts
+++ src/command/fetch/batch/collection.ts
@@ -3,6 +3,7 @@ import MCollection from '~/src/model/collection'
 import Base from '~/src/command/fetch/batch/base'
 import CommonUtil from '~/src/library/util/common'
 import BatchFetchAnswer from '~/src/command/fetch/batch/answer'
+import Logger from '~/src/library/logger'
 
 class BatchFetchCollection extends Base {
   async fetch(id: string) {
@@ -13,32 +14,35 @@ class BatchFetchCollection extends Base {
     let answerCount = collectionInfo.answer_count
     this.log(`话题${collectionInfo.title}(${collectionInfo.id})信息获取完毕, 共有回答${answerCount}个`)
 
-    let answerIdList: Array<string> = []
+    let answerIdList: string[] = []
     let batchFetchAnswer = new BatchFetchAnswer()
+    // @todo 收藏夹目前支持收藏文章和想法, 需要根据收藏类型进行分类处理. 数据库表结构也需要改一下
     this.log(`开始抓取回答列表`)
     for (let offset = 0; offset < answerCount; offset = offset + this.max) {
-      await this.asyncGetCollectionAnswerList(id, offset, answerIdList)
-      this.log(`将抓取第${offset}~${offset + this.max}个回答任务添加到任务队列中`)
-    }
-    await CommonUtil.asyncDispatchAllPromiseInQueen()
-    this.log(`全部回答列表抓取完毕`)
-
-    this.log(`开始抓取收藏夹${collectionInfo.title}(${collectionInfo.id})的下所有回答,共${answerIdList.length}条`)
-    await batchFetchAnswer.fetchListAndSaveToDb(answerIdList)
-    this.log(`收藏夹${collectionInfo.title}(${collectionInfo.id})下所有回答抓取完毕`)
-  }
-
-  private async asyncGetCollectionAnswerList(id: number | string, offset: number, answerIdList: Array<string>) {
-    let answerList = await CollectionApi.asyncGetAnswerExcerptList(id, offset, this.max)
-    for (let answer of answerList) {
-      // 传递给外部
-      answerIdList.push(`${answer.id}`)
-      await MCollection.asyncReplaceColumnAnswerExcerpt(id, answer).catch((e) => {
-        console.log('catch error')
-        console.log(e)
+      let asyncTaskFunc = async () => {
+        // 先拿到AnswerExcerpt, 然后再去获取回答详情
+        let answerList = await CollectionApi.asyncGetAnswerExcerptList(id, offset, this.max)
+        for (let answer of answerList) {
+          answerIdList.push(`${answer.id}`)
+          await MCollection.asyncReplaceColumnAnswerExcerpt(id, answer).catch((e) => {
+            Logger.log('catch error')
+            Logger.log(e)
+          })
+        }
+        this.log(`列表中第${offset}~${offset + answerList.length}条回答摘要抓取完毕`)
+      }
+      CommonUtil.addAsyncTaskFunc({
+        asyncTaskFunc,
+        needProtect: true,
+        label: this,
       })
     }
-    this.log(`列表中第${offset}~${offset + answerList.length}条回答抓取完毕`)
+    await CommonUtil.asyncWaitAllTaskCompleteByLabel(this)
+    this.log(`全部回答摘要列表抓取完毕`)
+
+    this.log(`开始抓取收藏夹${collectionInfo.title}(${collectionInfo.id})的下所有回答详情,共${answerIdList.length}条`)
+    await batchFetchAnswer.fetchListAndSaveToDb(answerIdList)
+    this.log(`收藏夹${collectionInfo.title}(${collectionInfo.id})下所有回答详情抓取完毕`)
   }
 }
 
