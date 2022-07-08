fb756ebf6ad232987c3372da7e0e052f86b231f1
yaozeyuan001 <yaozeyuan001@ke.com>
2022-07-08 23:52:56 +0800

    feat: 开始检查model, 并删除冗余代码

--- src/model/activity.ts
+++ src/model/activity.ts
@@ -37,43 +37,6 @@ class Activity extends Base {
   static TABLE_COLUMN = [`id`, `url_token`, `verb`, `raw_json`]
 
   /**
-   * 从数据库中获取指定用户指定类别的行为列表
-   * @param id
-   */
-  static async asyncGetActivityList(
-    urlToken: string,
-    startAt: number,
-    endAt: number,
-    verbList = [Activity.VERB_ANSWER_VOTE_UP, Activity.VERB_MEMBER_VOTEUP_ARTICLE],
-  ): Promise<TypeActivity.Record[]> {
-    let recordList = await this.db
-      .select(this.TABLE_COLUMN)
-      .from(this.TABLE_NAME)
-      .where('id', '>=', startAt)
-      .where('id', '<=', endAt)
-      .where('url_token', '=', urlToken)
-      .whereIn('verb', verbList)
-      .catch(() => {
-        return []
-      })
-
-    let activityRecordList = []
-    for (let record of recordList) {
-      let activityRecordJson = _.get(record, ['raw_json'], '{}')
-      let activityRecord
-      try {
-        activityRecord = JSON.parse(activityRecordJson)
-      } catch {
-        activityRecord = {}
-      }
-      if (_.isEmpty(activityRecord) === false) {
-        activityRecordList.push(activityRecord)
-      }
-    }
-    return activityRecordList
-  }
-
-  /**
    * 从数据库中获取指定用户指定类别的目标id列表
    * @param id
    */
@@ -90,10 +53,9 @@ class Activity extends Base {
         return []
       })
 
-    let activityRecordList = []
     let activityTargetIdList = []
     for (let record of recordList) {
-      let activityRecordJson = _.get(record, ['raw_json'], '{}')
+      let activityRecordJson = record?.raw_json
       let activityRecord: TypeActivity.Record
       try {
         activityRecord = JSON.parse(activityRecordJson)
@@ -123,46 +85,6 @@ class Activity extends Base {
       raw_json,
     })
     return
-  }
-
-  /**
-   * 简单统计用户各类行为次数
-   * @param urlToken
-   */
-  static async asyncGetActionSummary(urlToken: string) {
-    let rawRecordList = await this.db
-      .count('* as verb_count')
-      .select('verb')
-      .from(this.TABLE_NAME)
-      .where('url_token', '=', urlToken)
-      .groupBy('verb')
-      .catch(() => {
-        return []
-      })
-    let record = {}
-    for (let rawRecord of rawRecordList) {
-      let verb = _.get(rawRecord, ['verb'], '')
-      let count = _.get(rawRecord, ['verb_count'], 0)
-      _.set(record, [verb], count)
-    }
-    return record
-  }
-
-  /**
-   * 简单统计用户行为时间
-   * @param urlToken
-   */
-  static async asyncGetActionDuring(urlToken: string) {
-    let recordList = await this.db
-      .max('id as activity_end_at')
-      .min('id as activity_start_at')
-      .from(this.TABLE_NAME)
-      .where('url_token', '=', urlToken)
-      .catch(() => {
-        return []
-      })
-    let record = _.get(recordList, [0], {})
-    return record
   }
 }
 
