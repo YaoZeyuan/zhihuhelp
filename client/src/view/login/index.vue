191cbf20f6b37bdd1f0db60551b317307c053124
yaozeyuan <yaozeyuan93@gmail.com>
2022-10-31 23:26:38 +0800

    feat: 添加ua&启动调试工具按钮

--- client/src/view/login/index.vue
+++ client/src/view/login/index.vue
@@ -1,22 +1,47 @@
 <template>
-  <div>
-    <webview name="zhihu" id="foo" src="https://www.zhihu.com/" style></webview>
+  <div class="login-container">
+    <div class="item">
+      <button @click="openDevtools">打开调试工具</button>
+    </div>
+    <div class="item">
+      <webview
+        name="zhihu"
+        id="zhihu-login"
+        src="https://www.zhihu.com/"
+        useragent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
+        style
+      ></webview>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-  import _ from "lodash"
+import _ from 'lodash'
 
-  export default defineComponent({
-    name: 'dashboard',
-    data(){
-        return {}
-    }
-  })
+export default defineComponent({
+  name: 'dashboard',
+  data() {
+    return {}
+  },
+  methods: {
+    openDevtools() {
+      let webviewEle = document.querySelector('webview#zhihu-login')
+      console.log('webviewEle => ', webviewEle)
+      // @ts-ignore
+      webviewEle?.openDevTools()
+    },
+  },
+})
 </script>
 
 <style scoped>
+.login-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
 webview {
   display: inline-flex;
   width: 80vw;
