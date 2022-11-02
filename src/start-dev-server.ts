dcc247bdc60d768ab92d97b37d2667909715bdde
yaozeyuan <yaozeyuan93@gmail.com>
2022-11-02 22:37:48 +0800

    feat: debug时需要主动载入cookie

--- src/start-dev-server.ts
+++ src/start-dev-server.ts
@@ -1,6 +1,8 @@
 import { startServer } from './server'
+import RequestConfig from '~/src/config/request'
 
 let runner = async () => {
+  RequestConfig.reloadTaskConfig()
   console.log('start server')
   startServer({ port: 6370 })
   console.log('start server complete')
