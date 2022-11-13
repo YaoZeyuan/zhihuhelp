5b26d2044c2ccbf7a516a7c8abb87bea8cf1724b
yaozeyuan <yaozeyuan93@gmail.com>
2022-11-13 11:52:15 +0800

    feat: 添加js-rpc调试代码

--- src/public/js-rpc/renderer.js
+++ src/public/js-rpc/renderer.js
@@ -10,3 +10,14 @@ window.electronAPI.onUpdateCounter((event, value, id) => {
     value,
   })
 })
+
+// 只能通过这种方式执行js文件中的函数
+// 参考: https://stackoverflow.com/questions/36324333/refused-to-execute-inline-event-handler-because-it-violates-csp-sandbox
+document.getElementById('open-js-rpc-devtools').addEventListener('click', openRpcDevtools)
+function openRpcDevtools() {
+  let webviewEle = document.querySelector('webview#zhihuhelp-rpc')
+  console.log('webviewEle => ', webviewEle)
+  // @ts-ignore
+  webviewEle.openDevTools()
+  console.log('send success')
+}
