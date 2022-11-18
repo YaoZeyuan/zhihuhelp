a5ed23cf04a1a44b201bdacf2c93799e33b23404
yaozeyuan <yaozeyuan93@gmail.com>
2022-11-18 21:17:10 +0800

    feat: 删除冗余代码

--- src/router/api/host/index.ts
+++
@@ -1,5 +0,0 @@
-import RefreshCookie from '~/src/router/api/host/refresh_cookie/index'
-
-export default {
-  ...RefreshCookie,
-}
--- src/router/api/index.ts
+++
@@ -1,9 +0,0 @@
-import Host from '~/src/router/api/host'
-import Demo from '~/src/router/api/demo'
-import ZhihuHack from '~/src/router/api/zhihu_hack'
-
-export default {
-  ...Host,
-  ...Demo,
-  ...ZhihuHack,
-}
