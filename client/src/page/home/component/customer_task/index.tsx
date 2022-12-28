80b2b1c881eec83e8cd7f36e5e3d3f0503376c20
yaozeyuan <yaozeyuan93@gmail.com>
2022-12-28 22:52:24 +0800

    feat: 添加类型声明

--- client/src/page/home/component/customer_task/index.tsx
+++ client/src/page/home/component/customer_task/index.tsx
@@ -1,5 +1,5 @@
 import electron from 'electron'
-import { Button, message, Input, Form, Table, Modal, Tag, Card, Radio } from 'antd'
+import { Button, message, Input, Form, Table, Modal, Tag, Card, Radio, Select } from 'antd'
 import { proxy, useSnapshot } from 'valtio'
 
 import { useState, useContext, useEffect } from 'react'
@@ -10,17 +10,18 @@ import * as Types from './resource/type/index'
 import { store } from './state'
 import http from '~/src/library/http'
 
-import moment from 'moment'
-
 import './index.less'
 
 const { ipcRenderer } = electron
+const { Option } = Select
 
 export const Const_Storage_Key = 'login_msk'
 const Const_Table_Column_Width = 100
 
 export default () => {
   let snap = useSnapshot(store)
+
+  const [form] = Form.useForm()
 
   let [forceUpdate, setForceUpdate] = useState<number>(0)
 
@@ -34,6 +35,14 @@ export default () => {
     }
     asyncRunner()
   }, [forceUpdate])
+
+  const onFinish = (values: any) => {
+    console.log(values)
+  }
+
+  const onReset = () => {
+    form.resetFields()
+  }
 
   return (
     <div className="customer_task">
@@ -84,7 +93,44 @@ export default () => {
           调试-清除session
         </Button>
       </div>
-      <div className="config-panel"></div>
+      <div className="action-panel">{/* 操作栏 */}</div>
+      <div className="config-panel">
+        {/* 任务配置 */}
+        <Button>添加任务</Button>
+        <Form form={form} name="control-hooks" onFinish={onFinish}>
+          <Form.Item name="note" label="Note" rules={[{ required: true }]}>
+            <Input />
+          </Form.Item>
+          <Form.Item name="gender" label="Gender" rules={[{ required: true }]}>
+            <Select placeholder="Select a option and change input text above" allowClear>
+              <Option value="male">male</Option>
+              <Option value="female">female</Option>
+              <Option value="other">other</Option>
+            </Select>
+          </Form.Item>
+          <Form.Item noStyle shouldUpdate={(prevValues, currentValues) => prevValues.gender !== currentValues.gender}>
+            {({ getFieldValue }) =>
+              getFieldValue('gender') === 'other' ? (
+                <Form.Item name="customizeGender" label="Customize Gender" rules={[{ required: true }]}>
+                  <Input />
+                </Form.Item>
+              ) : null
+            }
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+            <Button htmlType="button" onClick={onReset}>
+              Reset
+            </Button>
+            <Button type="link" htmlType="button">
+              Fill form
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+      <div className="debug-panel">{/* 调试栏 */}</div>
     </div>
   )
 }
--- client/src/page/home/component/customer_task/resource/const/index.ts
+++ client/src/page/home/component/customer_task/resource/const/index.ts
@@ -1,2 +1,27 @@
 import * as Types from '../type'
 
+// 作为常量
+export const CONST_Task_Type_用户提问过的所有问题 = 'author-ask-question' as const
+export const CONST_Task_Type_用户的所有回答 = 'author-answer' as const
+export const CONST_Task_Type_用户发布的所有文章 = 'author-article' as const
+export const CONST_Task_Type_用户发布的所有想法 = 'author-pin' as const
+export const CONST_Task_Type_用户赞同过的所有回答 = 'author-agree-answer' as const
+export const CONST_Task_Type_用户赞同过的所有文章 = 'author-agree-article' as const
+export const CONST_Task_Type_用户关注过的所有问题 = 'author-watch-question' as const
+export const CONST_Task_Type_封号用户的所有回答 = 'block-account-answer' as const
+export const CONST_Task_Type_话题 = 'topic' as const
+export const CONST_Task_Type_收藏夹 = 'collection' as const
+export const CONST_Task_Type_专栏 = 'column' as const
+export const CONST_Task_Type_文章 = 'article' as const
+export const CONST_Task_Type_问题 = 'question' as const
+export const CONST_Task_Type_回答 = 'answer' as const
+export const CONST_Task_Type_想法 = 'pin' as const
+export const CONST_Order_By_创建时间 = 'createAt' as const
+export const CONST_Order_By_更新时间 = 'updateAt' as const
+export const CONST_Order_By_赞同数 = 'voteUpCount' as const
+export const CONST_Order_By_评论数 = 'commentCount' as const
+export const CONST_Order_Desc = 'desc' as const
+export const CONST_Order_Asc = 'asc' as const
+export const CONST_Image_Quilty_高清 = 'raw' as const
+export const CONST_Image_Quilty_原图 = 'hd' as const
+export const CONST_Image_Quilty_无图 = 'none' as const
