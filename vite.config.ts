2bb8b51449505c4e93b0c5321b4cf186c4eac077
yangmingming <yunmengze210@163.com>
2023-10-13 00:11:36 +0800

    feat: 解决publicPath问题

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    experimental: {
+        renderBuiltUrl(filename: string, { hostType }: { hostType: 'js' | 'css' | 'html' }) {
+            return `./${filename}`
+        }
+    }
 })
